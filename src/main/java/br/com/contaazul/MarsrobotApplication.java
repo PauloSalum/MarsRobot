1ee38e8133696fc85ed3184922083ba8996b97be
Paulo Magno Rodrigues Salum <paulo.salum@outlook.com>
2017-12-16 14:09:31 -0200

    Starter Commit.

---
+++ src/main/java/br/com/contaazul/MarsrobotApplication.java
@@ -0,0 +1,12 @@
+package br.com.contaazul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsrobotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarsrobotApplication.class, args);
+	}
+}
